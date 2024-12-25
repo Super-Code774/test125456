e21bcf82ab3f50a82b921535e7590511c3570673
Super-Code774 <kimonos.potiers-0f@icloud.com>
2024-12-25 11:35:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+KIOSK_URL="http://127.0.0.1:3000/NiclaSenseME-dashboard/index.html"
+
+
+sleep 8
+
+echo 'Hiding the mouse cursor...'
+unclutter -idle 0.1 -root &
+
+echo 'Starting Chromium...'
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --app=$KIOSK_URL#!/bin/bash
+
+
+KIOSK_URL="http://127.0.0.1:3000/NiclaSenseME-dashboard/index.html"
+
+
+sleep 8
+
+echo 'Hiding the mouse cursor...'
+unclutter -idle 0.1 -root &
+
+echo 'Starting Chromium...'
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --app=$KIOSK_URL
